18c4c92073f4bc7d48168e9401e1fee972268042
MichealSamuel <samuelmicheal306@gmail.com>
2025-10-14 00:01:25 +0100

    Fix import paths to pages folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PricingPage from './pages/PricingPage';
+import DashboardPage from './pages/DashboardPage';
+import SupportPage from './pages/SupportPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
