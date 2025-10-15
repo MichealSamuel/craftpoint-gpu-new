171074a57816306d7e1d6a9ceb0ecccc998b139d
MichealSamuel <samuelmicheal306@gmail.com>
2025-10-15 13:07:54 +0100

    Add @vercel/analytics package and update App.js import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 import HomePage from './pages/HomePage';
 import PricingPage from './pages/PricingPage';
 import DashboardPage from './pages/DashboardPage';
